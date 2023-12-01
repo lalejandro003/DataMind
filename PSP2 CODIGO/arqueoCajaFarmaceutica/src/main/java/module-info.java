9769d47e02df795f0631d639f931cb596976564e
LUIS ALEJANDRO <51064808+lalejandro003@users.noreply.github.com>
2023-11-30 23:05:40 -0600

    Add files via upload

---
+++ PSP2 CODIGO/arqueoCajaFarmaceutica/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module Controladores {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.base;
+    requires java.sql;
+
+    opens Controladores to javafx.fxml;
+    exports Controladores;
+    opens modelos;//Importar los modelos a todo el sistema
+}
